4d72c46a4a026db850f08bcc63ee588497591823
Nathan Klapstein <nklapste@ualberta.ca>
2018-11-23 20:01:30 -0700

    renaming `main.cpp` to `a4tasks.cpp`

--- src/a4tasks.cpp
+++ src/a4tasks.cpp
@@ -1,7 +1,7 @@
 // Copyright 2018 Nathan Klapstein
 
 /**
- * main.cpp
+ * a4tasks.cpp
  *
  * @author Nathan Klapstein (nklapste)
  * @version 0.0.0
@@ -9,7 +9,24 @@
 
 #include <iostream>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+/**
+ * Main entry point for a4tasks.
+ *
+ * Parse entry point arguments and startup main task functionality.
+ *
+ * Command format:
+ *      ``./a4tasks inputFile monitorTime NITER``
+ *
+ * @param argc {@code int}
+ * @param argv {@code char **}
+ * @return {@code int}
+ */
+int main(int argc, char **argv) {
+    if (argc < 3 || argc > 4) {
+        printf("ERROR: invalid argument format:\n"
+               "\tPlease follow: 'a4tasks inputFile monitorTime NITER'\n");
+        exit(EINVAL);
+    }
+
     return 0;
 }
