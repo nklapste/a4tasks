09e5cba03d329133733c94845aad4e0615f72655
Nathan Klapstein <nklapste@ualberta.ca>
2018-11-25 12:25:37 -0700

    adding base task thread components

--- src/task_thread.cpp
+++ src/task_thread.cpp
@@ -8,3 +8,51 @@
  */
 
 #include "task_thread.h"
+
+/**
+ * Getter the a {@code TaskThread}'s {@code TaskID}.
+ *
+ * @return {@code TaskID}
+ */
+TaskID TaskThread::getTaskID() {
+    return taskID;
+}
+
+/**
+ * Getter the a {@code TaskThread}'s ID number.
+ *
+ * @return {@code pthread_t}
+ */
+pthread_t TaskThread::getTID() {
+    return tid;
+}
+
+/**
+ * Getter the a {@code TaskThread}'s iteration number.
+ *
+ * @return {@code uint}
+ */
+uint TaskThread::getIterNum() {
+    return iterNum;
+}
+
+/**
+ * Getter the a {@code TaskThread}'s time.
+ *
+ * @return {@code std::chrono::milliseconds}
+ */
+milliseconds TaskThread::getTime() {
+    return time;
+}
+
+/**
+ * After a successful iteration a {@code TaskThread} is required to print
+ * the following message.
+ */
+void TaskThread::completePrint() {
+    printf("INFO: task: %s (tid= %lu, iter= %u, time=%limsec",
+           getTaskID().getTaskIDString().c_str(),
+           getTID(),
+           getIterNum(),
+           getTime().count());
+}
--- src/task_thread.h
+++ src/task_thread.h
@@ -10,11 +10,44 @@
 #ifndef A4TASKS_TASK_THREAD_H
 #define A4TASKS_TASK_THREAD_H
 
+#include <chrono>
 
 #include "managed_thread.h"
+#include "task_id.h"
+
+
+using std::chrono::milliseconds;
 
 class TaskThread : public ManagedThread {
+public:
+    TaskID getTaskID();
 
+    pthread_t getTID();
+
+    uint getIterNum();
+
+    milliseconds getTime();
+
+    void completePrint();
+
+private:
+    TaskID taskID;
+
+    /**
+     * A unsigned long integer (in the lab environ) noting the threads ID.
+     */
+    pthread_t tid;
+
+    /**
+     * The iteration number the thread has just completed.
+     */
+    uint iterNum;
+
+    /**
+     * The time in milliseconds relative to the start time of the a4tasks
+     * program.
+     */
+    milliseconds time;
 };
 
 
